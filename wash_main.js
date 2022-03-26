d944be41886b9656c1d15e19f8e55152e83991a0
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2022-03-26 14:02:11 +0530

    Add files via upload

--- wash_main.js
+++ wash_main.js
@@ -1,3 +1,5 @@
+img = "";
+status = "";
 function back() {
     window.location = "index.html";
 }
@@ -7,7 +9,23 @@ function preload() {
 function setup() {
    canvas = createCanvas(500, 410);
    canvas.center();
+
+   object_detector = ml5.objectDetector("cocossd", modelLoaded);
+   document.getElementById("status_wash").innerHTML = "Status: Detecting Objects";
 }
 function draw() {
 image(img, 0, 0, 500, 410);
+}
+function modelLoaded() {
+   console.log("Model is loaded!!");
+   status = true;
+   object_detector.detect(img, gotResults);
+}
+function gotResults(results) {
+   if(error) {
+      console.error(error);
+   }
+   else{
+      console.log(results);
+   }
 }
