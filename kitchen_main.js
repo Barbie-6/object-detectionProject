e09f6a30979715ac47cd86f528d189058ad99aaa
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2022-03-29 18:16:06 +0530

    Add files via upload

--- kitchen_main.js
+++ kitchen_main.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 function back() {
     window.location = "index.html";
 }
@@ -7,25 +8,41 @@ function preload() {
    img = loadImage("kitchen.png"); 
 }
 function setup() {
-   canvas = createCanvas(500, 410);
+   canvas = createCanvas(500, 370);
    canvas.center();
 
    object_detector = ml5.objectDetector("cocossd", modelLoaded);
    document.getElementById("status_kitchen").innerHTML = "Status: Detecting Objects";
 }
 function draw() {
-image(img, 0, 0, 500, 410);
+image(img, 0, 0, 500, 370);
+if(status != ""){
+   document.getElementById("status_kitchen").innerHTML = "Status: Objects Detected";
+   for(i=0; i<objects.length; i++) {
+      label = objects[i].label;
+      percentage = floor(objects[i].confidence * 100);
+      stroke("red");
+      noFill();
+      strokeWeight(2.5);
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      stroke("green");
+      strokeWeight(1);
+      text(label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+   }
+   document.getElementById("number_of_objects").innerHTML = "There are 11 objects in which " + objects.length + " has been detected";
+}
 }
 function modelLoaded() {
    console.log("Model is loaded!!");
    status = true;
    object_detector.detect(img, gotResults);
 }
-function gotResults(results) {
+function gotResults(error, results) {
    if(error) {
       console.error(error);
    }
    else{
       console.log(results);
+      objects = results;
    }
 }
